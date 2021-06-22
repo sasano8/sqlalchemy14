e4036d2aca74efb6486b74f64559a0a6efeb0b35
sasano8 <y-sasahara@ys-method.com>
2021-06-22 09:51:02 +0900

    modify github actions

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -3,6 +3,16 @@ import os
 import pytest
 
 
+@pytest.fixture(scope="session")
+def db_config():
+    host = os.getenv("POSTGRES_HOST", None)
+    db = os.getenv("POSTGRES_DB", None)
+    user = os.getenv("POSTGRES_USER", None)
+    pw = os.getenv("POSTGRES_PASSWORD", None)
+
+    return host, db, user, pw
+
+
 @pytest.mark.docker
 def test_integrate(request):
     env = request.config.getoption("--env")
@@ -10,11 +20,8 @@ def test_integrate(request):
 
 
 @pytest.mark.docker
-def test_env(request):
-    host = os.getenv("POSTGRES_HOST", None)
-    db = os.getenv("POSTGRES_DB", None)
-    user = os.getenv("POSTGRES_USER", None)
-    pw = os.getenv("POSTGRES_PASSWORD", None)
+def test_env(db_config):
+    host, db, user, pw = db_config
 
     assert host
     assert db
